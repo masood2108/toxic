a0d24a7c1a59a235957de3794e02b24549ebd81a
Masood Hussain <masoodhussain@Masoods-MacBook-Air.local>
2026-03-01 00:35:20 +0530

    show backend error message

--- api/send-otp.js
+++ api/send-otp.js
@@ -37,8 +37,10 @@ export default async function handler(req, res) {
 
     return res.status(200).json({ success: true })
 
-  } catch (err) {
-    console.error("SEND OTP ERROR", err)
-    return res.status(500).json({ success: false })
-  }
+  }catch (err) {
+  console.error("SEND OTP ERROR:", err)
+  return res.status(500).json({
+    success: false,
+    message: err.message
+  })
 }
